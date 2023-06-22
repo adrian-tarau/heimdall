68cd8c8c5f721b610d77b232ba07f9d05b7d1685
alex-tarau <alex@tarau.net>
2023-06-21 20:00:42 -0400

    Finish testing the SnmpService.

--- protocols/snmp/src/test/java/net/microfalx/heimdall/protocol/snmp/SnmpServiceTest.java
+++ protocols/snmp/src/test/java/net/microfalx/heimdall/protocol/snmp/SnmpServiceTest.java
@@ -8,6 +8,7 @@ import net.microfalx.heimdall.protocol.core.ProtocolSimulatorProperties;
 import net.microfalx.heimdall.protocol.core.jpa.AddressRepository;
 import net.microfalx.heimdall.protocol.core.jpa.PartRepository;
 import net.microfalx.heimdall.protocol.snmp.jpa.SnmpEventRepository;
+import net.microfalx.resource.FileResource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -17,9 +18,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.URI;
 import java.net.UnknownHostException;
 import java.time.ZonedDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ContextConfiguration(classes = {ProtocolConfiguration.class, ProtocolSimulatorProperties.class,
         SnmpProperties.class, SnmpService.class})
@@ -57,10 +62,21 @@ public class SnmpServiceTest extends AbstractBootstrapServiceTestCase {
     }
 
     @Test
-    void handle() {
+    void handle() throws IOException {
         ArgumentCaptor<net.microfalx.heimdall.protocol.snmp.jpa.SnmpEvent> snmpEventArgumentCaptor =
                 ArgumentCaptor.forClass(net.microfalx.heimdall.protocol.snmp.jpa.SnmpEvent.class);
         snmpService.handle(snmpEvent);
         Mockito.verify(snmpEventRepository).save(snmpEventArgumentCaptor.capture());
+        assertEquals(snmpEvent.getSource().getValue(), snmpEventArgumentCaptor.getValue().getAgentAddress().getValue());
+        assertEquals(snmpEvent.getSource().getName(), snmpEventArgumentCaptor.getValue().getAgentAddress().getName());
+        assertEquals(snmpEvent.getSource().getType(), snmpEventArgumentCaptor.getValue().getAgentAddress().getType());
+        assertEquals(snmpEvent.getCommunity(), snmpEventArgumentCaptor.getValue().getCommunityString());
+        assertEquals(snmpEvent.getEnterprise(), snmpEventArgumentCaptor.getValue().getEnterprise());
+        assertEquals(String.valueOf(snmpEvent.getVersion()), snmpEventArgumentCaptor.getValue().getVersion());
+        assertEquals(snmpEvent.getBodyAsString(), FileResource.create(URI.create(snmpEventArgumentCaptor.getValue().getBindingPart().getResource())).loadAsString());
+        assertEquals(snmpEvent.getCreatedAt().toLocalDateTime(), snmpEventArgumentCaptor.getValue().getCreatedAt());
+        assertEquals(snmpEvent.getReceivedAt().toLocalDateTime(), snmpEventArgumentCaptor.getValue().getReceivedAt());
+        assertEquals(snmpEvent.getSentAt().toLocalDateTime(), snmpEventArgumentCaptor.getValue().getSentAt());
+        assertEquals("SNMP Trap", snmpEvent.getName());
     }
 }
