c74c2fe95f922cde50d5b53a13d2c49cfebac97b
Adrian Tarau <adrian.tarau@gmail.com>
2023-08-22 19:36:14 -0400

    Update schema for protocol_snmp_mibs

--- protocols/snmp/src/main/resources/db/migration/mysql/V2.4__Create_Heimdall_Snmp.sql
+++ protocols/snmp/src/main/resources/db/migration/mysql/V2.4__Create_Heimdall_Snmp.sql
@@ -16,20 +16,20 @@ create table protocol_snmp_events
 
 create table protocol_snmp_mibs
 (
-    id              integer       not null auto_increment primary key,
-    name            varchar(100)  not null,
-    `type`          enum('SYSTEM','USER') not null,
-    module_id       varchar(200) not null,
-    message_oid     varchar(200),
-    enterprise_oid  varchar(200),
-    create_at_oid   varchar(200),
-    sent_at_oid     varchar(200),
-    severity_oid    varchar(200),
-    created_at      datetime      not null,
-    modified_at     datetime      not null,
-    file_name       varchar(100)  not null,
-    `content`         mediumtext,
-    description     varchar(1000)
-)ENGINE = InnoDB;
+    id             integer                not null auto_increment primary key,
+    name           varchar(100)           not null,
+    `type`         enum ('SYSTEM','USER') not null,
+    module_id      varchar(200)           not null,
+    enterprise_oid varchar(200),
+    message_oids   varchar(5000),
+    create_at_oids varchar(5000),
+    sent_at_oids   varchar(5000),
+    severity_oids  varchar(5000),
+    created_at     datetime               not null,
+    modified_at    datetime               not null,
+    file_name      varchar(100)           not null,
+    `content`      mediumtext,
+    description    varchar(1000)
+) ENGINE = InnoDB;
 
 create index protocol_snmp_mibs$module_id on protocol_snmp_mibs (module_id);
