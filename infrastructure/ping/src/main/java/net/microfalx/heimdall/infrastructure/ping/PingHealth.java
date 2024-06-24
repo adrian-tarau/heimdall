0201767d291a064a17f26b28875bade24d9f2f38
Alex Tarau <alex@tarau.net>
2024-06-24 14:37:02 -0400

    Start testing the PingHealth

--- infrastructure/ping/src/main/java/net/microfalx/heimdall/infrastructure/ping/PingHealth.java
+++ infrastructure/ping/src/main/java/net/microfalx/heimdall/infrastructure/ping/PingHealth.java
@@ -7,11 +7,14 @@ import net.microfalx.heimdall.infrastructure.api.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
+
+import static java.util.Collections.unmodifiableMap;
 
 /**
  * A class which monitors the outcome of pings and calculates the health of services.
@@ -19,12 +22,22 @@ import java.util.concurrent.ConcurrentHashMap;
 @Component
 public class PingHealth {
 
-    private static final Queue<Ping> EMPTY_PINGS = new LinkedList<>();
+    private static final Deque<Ping> EMPTY_PINGS = new LinkedList<>();
 
     @Autowired
     private PingProperties properties;
 
     private final Map<String, Queue<Ping>> pings = new ConcurrentHashMap<>();
+    private final Map<String, Ping> lastPings = new ConcurrentHashMap<>();
+
+    /**
+     * Returns the tracked pings.
+     *
+     * @return a non-null instance
+     */
+    Map<String, Queue<Ping>> getPings() {
+        return unmodifiableMap(pings);
+    }
 
     /**
      * Registers the outcome of a ping.
@@ -35,6 +48,7 @@ public class PingHealth {
         Queue<Ping> queue = pings.computeIfAbsent(ping.getId(), pings -> new ArrayBlockingQueue<>(properties.getWindowSize()));
         if (queue.offer(ping)) queue.remove();
         queue.offer(ping);
+        lastPings.put(ping.getId(), ping);
     }
 
     /**
@@ -45,8 +59,7 @@ public class PingHealth {
      * @return a non-null status
      */
     Status getStatus(net.microfalx.heimdall.infrastructure.api.Service service, Server server) {
-        Queue<Ping> queue = getPingQueue(service, server);
-        Ping ping = queue.peek();
+        Ping ping = lastPings.get(PingUtils.getId(service, server));
         return ping == null ? Status.NA : ping.getStatus();
     }
 
@@ -55,20 +68,30 @@ public class PingHealth {
         if (queue.isEmpty()) return Health.NA;
         int numberOfFailPings = 0;
         for (Ping ping : queue) {
+            System.out.println("Ping hash: "+ping.hashCode()+", status: "+ping.getStatus());
             if (ping.getStatus().isFailure()) numberOfFailPings++;
         }
-        if (queue.size() != properties.getWindowSize() && numberOfFailPings == 0) return Health.HEALTHY;
-        int percentageOfFailPings = (numberOfFailPings / properties.getWindowSize()) * 100;
-        if (percentageOfFailPings >= properties.getUnhealthyThreshold()) {
-            return Health.UNHEALTHY;
-        } else if (percentageOfFailPings >= properties.getDegradedThreshold()) {
-            return Health.DEGRADED;
+        if (queue.size() != properties.getWindowSize()) {
+            if (numberOfFailPings == 0) {
+                return Health.HEALTHY;
+            } else {
+                return Health.NA;
+            }
         } else {
-            return Health.HEALTHY;
+            float percentageOfFailPings = ((float) numberOfFailPings / properties.getWindowSize()) * 100;
+            if (percentageOfFailPings >= properties.getUnhealthyThreshold()) {
+                return Health.UNHEALTHY;
+            } else if (percentageOfFailPings >= properties.getDegradedThreshold()) {
+                return Health.DEGRADED;
+            } else {
+                return Health.HEALTHY;
+            }
         }
     }
 
     private Queue<Ping> getPingQueue(net.microfalx.heimdall.infrastructure.api.Service service, Server server) {
         return pings.getOrDefault(PingUtils.getId(service, server), EMPTY_PINGS);
     }
+
+
 }
--- infrastructure/ping/src/test/java/net/microfalx/heimdall/infrastructure/ping/PingHealthTest.java
+++ infrastructure/ping/src/test/java/net/microfalx/heimdall/infrastructure/ping/PingHealthTest.java
@@ -1,5 +1,122 @@
 package net.microfalx.heimdall.infrastructure.ping;
 
+import net.microfalx.heimdall.infrastructure.api.Ping;
+import net.microfalx.heimdall.infrastructure.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class PingHealthTest {
+
+    private Service service;
+    private Server server;
+
+    @Mock
+    private Ping ping;
+
+    @Spy
+    private PingProperties properties;
+
+    @InjectMocks
+    private PingHealth health;
+
+    @BeforeEach
+    void setUp() {
+        service = Service.create(Service.Type.ICMP);
+        server = new Server.Builder().hostname("localhost").build();
+        mockPing(Status.NA);
+    }
+
+    @Test
+    @MockitoSettings(strictness = Strictness.LENIENT)
+    void noPingRegistration() {
+        when(ping.getStatus()).thenReturn(Status.NA);
+        assertEquals(Status.NA, health.getStatus(service, server));
+        assertEquals(Health.NA, health.getHealth(service, server));
+    }
+
+    @Test
+    @MockitoSettings(strictness = Strictness.LENIENT)
+    void withPingRegistrationWithOneSuccessStatus() {
+        when(ping.getStatus()).thenReturn(Status.L3OK);
+        health.registerPing(ping);
+        assertEquals(1, health.getPings().values().iterator().next().size());
+        assertEquals(Status.L3OK, health.getStatus(service, server));
+        assertEquals(Health.HEALTHY, health.getHealth(service, server));
+    }
+
+    @Test
+    @MockitoSettings(strictness = Strictness.LENIENT)
+    void withPingRegistrationWithFullHealthyStatus() {
+        when(ping.getStatus()).thenReturn(Status.L3OK);
+        for (int i = 0; i < 100; i++) {
+            health.registerPing(ping);
+        }
+        mockPing(Status.L4OK);
+        health.registerPing(ping);
+        assertEquals(20, health.getPings().values().iterator().next().size());
+        assertEquals(Status.L4OK, health.getStatus(service, server));
+        assertEquals(Health.HEALTHY, health.getHealth(service, server));
+    }
+
+
+    @Test
+    @MockitoSettings(strictness = Strictness.LENIENT)
+    void withPingRegistrationWithOneFailureStatus() {
+        when(ping.getStatus()).thenReturn(Status.L3CON);
+        health.registerPing(ping);
+        assertEquals(1, health.getPings().values().iterator().next().size());
+        assertEquals(Status.L3CON, health.getStatus(service, server));
+        assertEquals(Health.NA, health.getHealth(service, server));
+    }
+
+    @Test
+    @MockitoSettings(strictness = Strictness.LENIENT)
+    void withPingRegistrationWithFullAndDegradedStatus() {
+        when(ping.getStatus()).thenReturn(Status.L3OK);
+        registerGoodPings();
+        mockPing(Status.L3CON);
+        health.registerPing(ping);
+        assertEquals(20, health.getPings().values().iterator().next().size());
+        assertEquals(Status.L3CON, health.getStatus(service, server));
+        assertEquals(Health.DEGRADED, health.getHealth(service, server));
+    }
+
+    @Test
+    @MockitoSettings(strictness = Strictness.LENIENT)
+    void withPingRegistrationWithFullAndUnhealthyStatus() {
+        when(ping.getStatus()).thenReturn(Status.L4TOUT);
+        registerGoodPings();
+        mockPing(Status.L7TOUT);
+        health.registerPing(ping);
+        mockPing(Status.L3CON);
+        health.registerPing(ping);
+        assertEquals(20, health.getPings().values().iterator().next().size());
+        assertEquals(Status.L3CON, health.getStatus(service, server));
+        assertEquals(Health.UNHEALTHY, health.getHealth(service, server));
+    }
+
+    private void registerGoodPings() {
+        for (int i = 0; i < 100; i++) {
+            health.registerPing(ping);
+        }
+    }
+
+    private void mockPing(Status status) {
+        ping = mock(Ping.class);
+        when(ping.getId()).thenReturn(PingUtils.getId(service, server));
+        when(ping.getStatus()).thenReturn(status);
+    }
 
 }
