4579fc9589081eb69272f0aea2992623a4d18132
Alex Tarau <alex@tarau.net>
2024-07-31 15:40:20 -0400

    Switch duration for pings to nanoseconds

--- infrastructure/ping/src/main/java/net/microfalx/heimdall/infrastructure/ping/overview/Ping.java
+++ infrastructure/ping/src/main/java/net/microfalx/heimdall/infrastructure/ping/overview/Ping.java
@@ -64,19 +64,19 @@ public class Ping extends NamedIdentityAware<String> {
 
     @Position(29)
     @Description("The amount of time the last health check took")
-    @Formattable(unit = Formattable.Unit.MILLI_SECOND)
+    @Formattable(unit = Formattable.Unit.NANO_SECOND)
     @Label(value = "Last", group = "Durations")
     private Duration lastDuration;
 
     @Position(30)
     @Description("The minimum time it took to run the health check")
-    @Formattable(unit = Formattable.Unit.MILLI_SECOND)
+    @Formattable(unit = Formattable.Unit.NANO_SECOND)
     @Label(value = "Minimum", group = "Durations")
     private Duration minimumDuration;
 
     @Position(31)
     @Description("The average time it took to run the health check")
-    @Formattable(unit = Formattable.Unit.MILLI_SECOND)
+    @Formattable(unit = Formattable.Unit.NANO_SECOND)
     @Label(value = "Average", group = "Durations")
     private Duration averageDuration;
 
