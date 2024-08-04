04261a6d20270bdb20461391d9587450b82248c0
Adrian Tarau <adrian.tarau@gmail.com>
2024-08-04 12:05:56 -0400

    Add @EnableJpaRepositories & @EntityScan

--- web/src/main/java/net/microfalx/heimdall/web/HeimdallApplication.java
+++ web/src/main/java/net/microfalx/heimdall/web/HeimdallApplication.java
@@ -2,10 +2,16 @@ package net.microfalx.heimdall.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ComponentScan({"net.microfalx.bootstrap", "net.microfalx.heimdall"})
+@EnableJpaRepositories({"net.microfalx.bootstrap", "net.microfalx.heimdall"})
+@EntityScan({"net.microfalx.bootstrap", "net.microfalx.heimdall"})
+@EnableTransactionManagement
 public class HeimdallApplication {
 
     public static void main(String[] args) {
