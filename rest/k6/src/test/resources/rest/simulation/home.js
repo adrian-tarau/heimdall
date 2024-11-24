7e7331e3553b6fe0e24010ee1c0bb7bd58af9249
Adrian Tarau <adrian.tarau@gmail.com>
2024-11-24 11:03:38 -0500

    Fix script errors

--- rest/k6/src/test/resources/rest/simulation/home.js
+++ rest/k6/src/test/resources/rest/simulation/home.js
@@ -2,13 +2,13 @@ import http from 'k6/http';
 import {sleep} from 'k6';
 
 export const options = {
-    duration: '2m',
     stages: [
         {duration: '30s', target: 1},
         {duration: '30s', target: 5},
         {duration: '20s', target: 0},
     ]
 };
+
 export default function () {
     http.get(`${__ENV.APP_URI}`);
     sleep(1 + 2 * Math.random());
--- rest/k6/src/test/resources/rest/simulation/not_found.js
+++ rest/k6/src/test/resources/rest/simulation/not_found.js
@@ -6,6 +6,7 @@ export const options = {
     iterations : 100,
     duration: '2m'
 };
+
 export default function () {
     http.get(`${__ENV.APP_URI}/not_found`);
     sleep(1 + 2 * Math.random());
