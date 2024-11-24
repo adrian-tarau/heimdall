4a4bfda3ca3ac5fbfbbef51ff2bb90e6bd70a024
Adrian Tarau <adrian.tarau@gmail.com>
2024-11-23 21:57:41 -0500

    Better simulation for main page

--- rest/k6/src/test/resources/rest/simulation/home.js
+++ rest/k6/src/test/resources/rest/simulation/home.js
@@ -2,10 +2,16 @@ import http from 'k6/http';
 import {sleep} from 'k6';
 
 export const options = {
-    vus: 2,
-    duration: '10s',
+    vus: 1,
+    iterations: 50,
+    duration: '2m',
+    stages: [
+        {duration: '30s', target: 1},
+        {duration: '30s', target: 5},
+        {duration: '20s', target: 0},
+    ]
 };
 export default function () {
     http.get(`${__ENV.REST_API_URI}`);
-    sleep(1);
+    sleep(1 + 2 * Math.random());
 }
