f515d35149d230377f94df68be5699f027c5fbb4
Adrian Tarau <adrian.tarau@gmail.com>
2024-11-24 10:58:45 -0500

    Better simulation for main page, add a simulation which results in 404

--- rest/k6/src/test/resources/rest/simulation/home.js
+++ rest/k6/src/test/resources/rest/simulation/home.js
@@ -2,8 +2,6 @@ import http from 'k6/http';
 import {sleep} from 'k6';
 
 export const options = {
-    vus: 1,
-    iterations: 50,
     duration: '2m',
     stages: [
         {duration: '30s', target: 1},
@@ -12,6 +10,6 @@ export const options = {
     ]
 };
 export default function () {
-    http.get(`${__ENV.REST_API_URI}`);
+    http.get(`${__ENV.APP_URI}`);
     sleep(1 + 2 * Math.random());
 }
---
+++ rest/k6/src/test/resources/rest/simulation/not_found.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import {sleep} from 'k6';
+
+export const options = {
+    vus : 1,
+    iterations : 100,
+    duration: '2m'
+};
+export default function () {
+    http.get(`${__ENV.APP_URI}/not_found`);
+    sleep(1 + 2 * Math.random());
+}
