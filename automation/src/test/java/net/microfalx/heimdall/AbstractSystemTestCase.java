1a5f4d5af0685dd6fd29e2d5a8ea895d15c13b11
Adrian Tarau <adrian.tarau@gmail.com>
2025-03-28 18:36:32 -0400

    Enable BootstrapExtension to perform additional pre- and post-activities around Selenium

--- automation/src/test/java/net/microfalx/heimdall/AbstractSystemTestCase.java
+++ automation/src/test/java/net/microfalx/heimdall/AbstractSystemTestCase.java
@@ -1,11 +1,12 @@
 package net.microfalx.heimdall;
 
+import net.microfalx.bootstrap.serenity.extension.BoostrapExtension;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.annotations.CastMember;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith(SerenityJUnit5Extension.class)
+@ExtendWith({BoostrapExtension.class, SerenityJUnit5Extension.class,})
 public abstract class AbstractSystemTestCase {
 
     @CastMember(name = "Toby")
