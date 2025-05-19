8c43591494560ed01ecc867c6083bb0f2402df1e
Alex Tarau <alex@tarau.net>
2025-05-18 20:13:08 -0400

    Register all the models for Ollama and JLama providers

--- llm/provider/ollama/src/main/java/net/microfalx/heimdall/llm/ollama/OllamaListener.java
+++ llm/provider/ollama/src/main/java/net/microfalx/heimdall/llm/ollama/OllamaListener.java
@@ -2,6 +2,7 @@ package net.microfalx.heimdall.llm.ollama;
 
 import net.microfalx.heimdall.llm.api.AiListener;
 import net.microfalx.heimdall.llm.api.AiService;
+import net.microfalx.heimdall.llm.api.Model;
 import net.microfalx.lang.annotation.Provider;
 
 @Provider
@@ -13,6 +14,13 @@ public class OllamaListener implements AiListener {
         builder.name("Ollama").description("Get up and running with large language models.");
         builder.version("0.7.0").author("Ollama Team").license("MIT")
                 .chatFactory(new OllamaChatFactory());
+        registerModels(builder);
         service.registerProvider(builder.build());
+    }
+
+    private void registerModels(net.microfalx.heimdall.llm.api.Provider.Builder builder) {
+        builder.model(Model.create("gemma3:4b","Gemma3 (4b)","/gemma3:4b"));
+        builder.model(Model.create("qwen3:0.6b","Qwen3 (0.6b)"));
+        builder.model(Model.create("deepseek-r1:7b","DeepSeek-R1 (7b)"));
     }
 }
