352181dd946d3a7b9b15bd4c906734e30206ae77
Alex Tarau <alex@tarau.net>
2025-05-21 16:09:33 -0400

    Tweak the model and provider entities

--- llm/core/src/main/java/net/microfalx/heimdall/llm/core/Model.java
+++ llm/core/src/main/java/net/microfalx/heimdall/llm/core/Model.java
@@ -6,7 +6,10 @@ import lombok.Setter;
 import net.microfalx.bootstrap.dataset.annotation.Filterable;
 import net.microfalx.bootstrap.jdbc.entity.NamedIdentityAware;
 import net.microfalx.heimdall.llm.api.ResponseFormat;
-import net.microfalx.lang.annotation.*;
+import net.microfalx.lang.annotation.Description;
+import net.microfalx.lang.annotation.Name;
+import net.microfalx.lang.annotation.NaturalId;
+import net.microfalx.lang.annotation.Width;
 
 @Entity
 @Table(name = "model")
@@ -16,62 +19,48 @@ import net.microfalx.lang.annotation.*;
 public class Model extends NamedIdentityAware<Integer> {
 
     @NaturalId
-    @Position(2)
     @Column(name = "natural_id", nullable = false, length = 100, unique = true)
     private String naturalId;
 
-    @Position(10)
     @ManyToOne
     @JoinColumn(name = "provider_id", nullable = false)
     private Provider provider;
 
-    @Position(9)
     @Column(name = "uri", length = 1000)
     private String uri;
 
-    @Position(10)
     @Column(name = "api_key", nullable = false, length = 500)
     private String apiKey;
 
-    @Position(15)
     @Column(name = "model_name", length = 100)
     private String modelName;
 
-    @Position(20)
     @Column(name = "temperature")
     private Double temperature;
 
-    @Position(25)
     @Column(name = "top_p")
     private Double topP;
 
-    @Position(30)
     @Column(name = "top_k")
     private Integer topK;
 
-    @Position(35)
     @Column(name = "frequency_penalty")
     private Double frequencyPenalty;
 
-    @Position(40)
     @Column(name = "presence_penalty")
     private Double presencePenalty;
 
-    @Position(45)
     @Column(name = "maximum_output_tokens")
     private Integer maximumOutputTokens;
 
-    @Position(50)
     @Column(name = "stop_sequences", nullable = false, length = 1000)
     private String stopSequences;
 
-    @Position(55)
     @Column(name = "response_format", nullable = false)
     @Enumerated(value = EnumType.STRING)
     private ResponseFormat responseFormat;
 
     @Column(name = "tags")
-    @Position(400)
     //@Component(Component.Type.TAG)
     @Description("A collection of tags associated with a {name}")
     @Width("150px")
--- llm/core/src/main/java/net/microfalx/heimdall/llm/core/Provider.java
+++ llm/core/src/main/java/net/microfalx/heimdall/llm/core/Provider.java
@@ -7,7 +7,10 @@ import lombok.Getter;
 import lombok.Setter;
 import net.microfalx.bootstrap.dataset.annotation.Filterable;
 import net.microfalx.bootstrap.jdbc.entity.NamedIdentityAware;
-import net.microfalx.lang.annotation.*;
+import net.microfalx.lang.annotation.Description;
+import net.microfalx.lang.annotation.Name;
+import net.microfalx.lang.annotation.NaturalId;
+import net.microfalx.lang.annotation.Width;
 
 @Entity
 @Table(name = "provider")
@@ -17,35 +20,28 @@ import net.microfalx.lang.annotation.*;
 public class Provider extends NamedIdentityAware<Integer> {
 
     @NaturalId
-    @Position(2)
     @Column(name = "natural_id", nullable = false, length = 100, unique = true)
     private String naturalId;
 
-    @Position(15)
     @Column(name = "uri", length = 1000)
     private String uri;
 
-    @Position(20)
     @Column(name = "api_key", nullable = false, length = 500)
     private String apiKey;
 
-    @Position(25)
     @Column(name = "author", nullable = false, length = 100)
     private String author;
 
     @Column(name = "tags")
-    @Position(400)
     //@Component(Component.Type.TAG)
     @Description("A collection of tags associated with a {name}")
     @Width("150px")
     @Filterable()
     private String tags;
 
-    @Position(500)
     @Column(name = "license",length = 1000)
     private String license;
 
-    @Position(600)
     @Column(name = "version",length = 50)
     private String version;
 }
