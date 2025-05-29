83befff75bd7bc02692c3c83c7869387636829a8
Alex Tarau <alex@tarau.net>
2025-05-29 14:44:48 -0400

    Add more models and the model name

--- llm/provider/github/src/main/java/net/microflax/heimdall/llm/github/GithubProviderFactory.java
+++ llm/provider/github/src/main/java/net/microflax/heimdall/llm/github/GithubProviderFactory.java
@@ -21,24 +21,25 @@ public class GithubProviderFactory implements Provider.Factory {
     }
 
     private void registerModels(net.microfalx.heimdall.llm.api.Provider.Builder builder) {
-        builder.model(Model.create("gpt_4o", "GPT 4o")).tag("openai").tag("gpt");
-        builder.model(Model.create("gpt_4.1", "GPT 4.1")).tag("openai").tag("gpt");
-        builder.model(Model.create("o1", "O1")).tag("openai");
-        builder.model(Model.create("o3", "O3")).tag("openai");
-        builder.model(Model.create("o3_mini", "O3 Mini")).tag("openai").tag("mini");
-        builder.model(Model.create("o4_mini", "O4 Mini")).tag("openai").tag("mini");
+        builder.model(Model.create("gpt_4o", "GPT 4o", "GPT-4o")).tag("openai").tag("gpt");
+        builder.model(Model.create("gpt_4.1", "GPT 4.1", "GPT-4.1")).tag("openai").tag("gpt");
+        builder.model(Model.create("o1", "O1", "o1")).tag("openai");
+        builder.model(Model.create("o3", "O3", "o3")).tag("openai");
+        builder.model(Model.create("o3_mini", "O3 Mini", "o3-mini")).tag("openai").tag("mini");
+        builder.model(Model.create("o4_mini", "O4 Mini", "o4-mini")).tag("openai").tag("mini");
 
-        builder.model(Model.create("claude_opus_4", "Claude Opus 4"))
+        builder.model(Model.create("claude_opus_4", "Claude Opus 4", "Claude Opus 4"))
                 .tag("anthropic").tag("claude").tag("opus");
-        builder.model(Model.create("claude_3.5_sonnet", "Claude 3.5 Sonnet"))
+        builder.model(Model.create("claude_3.5_sonnet", "Claude 3.5 Sonnet", "Claude 3.5 Sonnet"))
                 .tag("anthropic").tag("claude").tag("sonnet");
-        builder.model(Model.create("claude_3.7_sonnet", "Claude 3.7 Sonnet"))
+        builder.model(Model.create("claude_3.7_sonnet", "Claude 3.7 Sonnet", "Claude 3.7 Sonnet"))
                 .tag("anthropic").tag("claude").tag("sonnet");
-        builder.model(Model.create("claude_3.7_sonnet_thinking", "Claude 3.7 Sonnet Thinking"))
+        builder.model(Model.create("claude_3.7_sonnet_thinking", "Claude 3.7 Sonnet Thinking", "Claude 3.7 Sonnet Thinking"))
                 .tag("anthropic").tag("claude").tag("sonnet").tag("thinking");
-        builder.model(Model.create("claude_sonnet_4", "Claude Sonnet 4"))
+        builder.model(Model.create("claude_sonnet_4", "Claude Sonnet 4", "Claude Sonnet 4"))
                 .tag("anthropic").tag("claude").tag("sonnet");
-        builder.model(Model.create("gemini_2.0_flash", "Gemini 2.0 Flash"))
+
+        builder.model(Model.create("gemini_2.0_flash", "Gemini 2.0 Flash", "Gemini 2.0 Flash"))
                 .tag("google").tag("gemini").tag("flash");
 
     }
--- llm/provider/ollama/src/main/java/net/microfalx/heimdall/llm/ollama/OllamaProviderFactory.java
+++ llm/provider/ollama/src/main/java/net/microfalx/heimdall/llm/ollama/OllamaProviderFactory.java
@@ -4,7 +4,7 @@ import net.microfalx.heimdall.llm.api.Model;
 import net.microfalx.heimdall.llm.api.Provider;
 
 @net.microfalx.lang.annotation.Provider
-public class OllamaProviderFactory implements Provider.Factory{
+public class OllamaProviderFactory implements Provider.Factory {
 
     @Override
     public Provider createProvider() {
@@ -17,8 +17,17 @@ public class OllamaProviderFactory implements Provider.Factory{
     }
 
     private void registerModels(net.microfalx.heimdall.llm.api.Provider.Builder builder) {
-        builder.model(Model.create("gemma3:4b","Gemma3 (4b)")).tag("meta").tag("gemma");
-        builder.model(Model.create("qwen3:0.6b","Qwen3 (0.6b)")).tag("alibaba").tag("qwen3");
-        builder.model(Model.create("deepseek-r1:7b","DeepSeek-R1 (7b)")).tag("deepseek").tag("deepseek-r1");
+        builder.model(Model.create("gemma3_4b", "Gemma3 (4b)", "gemma3:4b")).tag("google").tag("gemma");
+        builder.model(Model.create("gemma3_1b", "Gemma3 (1b)", "gemma3:1b")).tag("google").tag("gemma");
+
+        builder.model(Model.create("qwen3_0.6b", "Qwen3 (0.6b)", "qwen3:0.6b")).tag("alibaba").tag("qwen3");
+        builder.model(Model.create("qwen3_1_7b", "Qwen3 (1.7b)", "qwen3:1.7b")).tag("alibaba").tag("qwen3");
+        builder.model(Model.create("qwen3_4b", "Qwen3 (4b)", "qwen3:4b")).tag("alibaba").tag("qwen3");
+
+        builder.model(Model.create("deepseek_r1_7b", "DeepSeek-R1 (7b)", "deepseek-r1:7b")).tag("deepseek").tag("deepseek-r1");
+        builder.model(Model.create("deepseek_r1_7b", "DeepSeek-R1 (7b)", "deepseek-r1:7b")).tag("deepseek").tag("deepseek-r1");
+
+        builder.model(Model.create("llama3_2_1b", "Llama 3.2 (1b)", "llama3.2:1b")).tag("meta").tag("llama");
+        builder.model(Model.create("llama3_2_3b", "Llama 3.2 (3b)", "llama3.2:3b")).tag("meta").tag("llama");
     }
 }
